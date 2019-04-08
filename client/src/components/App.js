9f61e34f55032bba923b130b470937418d429970
isaac1104 <isaac-kwon@hotmail.com>
2019-04-08 17:10:58 +0900

    add sidebar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,11 @@
 import React, { Component, lazy, Suspense } from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { Layout } from 'antd';
 import { fetchCurrentUserData } from '../actions';
 import requireAuth from '../utils/requireAuth';
+import ContentLayout from '../utils/ContentLayout'
+import Sidebar from './Sidebar';
 
 const Landing = lazy(() => import('./Landing'));
 const Home = lazy(() => import('./Home'));
@@ -15,23 +18,28 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Suspense fallback={<h3>Loading...</h3>}>
-          <Switch>
-            <Route
-              exact
-              path='/'
-              render={() => {
-                const { data } = this.props.currentUser;
-                if (data) {
-                  return <Redirect to='/home' />;
-                } else {
-                  return <Landing />;
-                }
-              }}
-            />
-            <Route exact path='/home' component={requireAuth(Home)} />
-          </Switch>
-        </Suspense>
+        <Layout>
+          <Sidebar />
+          <ContentLayout>
+            <Suspense fallback={<h3>Loading...</h3>}>
+              <Switch>
+                <Route
+                  exact
+                  path='/'
+                  render={() => {
+                    const { data } = this.props.currentUser;
+                    if (data) {
+                      return <Redirect to='/home' />;
+                    } else {
+                      return <Landing />;
+                    }
+                  }}
+                />
+                <Route exact path='/home' component={requireAuth(Home)} />
+              </Switch>
+            </Suspense>
+          </ContentLayout>
+        </Layout>
       </BrowserRouter>
     );
   }
---
+++ client/src/utils/ContentLayout.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Layout } from 'antd';
+const { Content } = Layout;
+
+class ContentLayout extends Component {
+  render() {
+    const { pathname } = window.location;
+    const style = {
+      layout: {
+        padding: '0 24px',
+        backgroundColor: '#f0f2f5',
+        height: '100vh',
+        overflow: 'hidden'
+      },
+      content: {
+        backgroundColor: '#ffffff',
+        padding: 24,
+        margin: 0,
+        width: '100%',
+        height: '100%',
+        overflowY: pathname === '/' ? 'hidden' : 'scroll'
+      }
+    };
+
+    return (
+      <Layout style={style.layout}>
+        <Content style={style.content}>
+          {this.props.children}
+        </Content>
+      </Layout>
+    );
+  }
+}
+
+export default ContentLayout;
