7ca94b1171432356a5943ec161ad413039d0648b
isaac1104 <isaac-kwon@hotmail.com>
2019-04-08 16:05:11 +0900

    add dev.js to gitignore

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  googleClientID: '635622312051-va3n9hvm5kdjim24r0ld7nvhe4v4d9ae.apps.googleusercontent.com',
-  googleClientSecret: 'bVZYBbK7Yt7BvWhs00ZL5Q75',
-  mongodbURI: 'mongodb://isaac1104:Kw1104on@ds233806.mlab.com:33806/medium-clone',
-  cookieKey: 'y0KpvzSsjC',
-};
