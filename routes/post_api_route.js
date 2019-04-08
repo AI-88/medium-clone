46c3307a4e3ab170d7f382e393a7205366c2c169
isaac1104 <isaac-kwon@hotmail.com>
2019-04-08 22:59:22 +0900

    protect api routes if not logged in

--- routes/post_api_route.js
+++ routes/post_api_route.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose');
+const requireAuth = require('../middlewares/requireAuth');
 const Posts = mongoose.model('Post');
 
 module.exports = app => {
-  app.get('/api/posts', async (req, res) => {
+  app.get('/api/posts', requireAuth, async (req, res) => {
     try {
       const posts = await Posts.find({});
-      res.status(200).send(posts)
+      res.status(200).send(posts);
     } catch (e) {
       res.sendStatus(404);
     }
   });
 
-  app.post('/api/posts', async (req, res) => {
+  app.post('/api/posts', requireAuth, async (req, res) => {
     const { title, coordinates, description } = req.body;
     const newPost = new Posts({
       title,
