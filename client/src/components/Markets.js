60cbac286a4cc507704fea0a6fed318c10251dbd
isaac1104 <isaac-kwon@hotmail.com>
2019-04-08 22:51:33 +0900

    make create post only for admin

--- client/src/components/Markets.js
+++ client/src/components/Markets.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPostsData } from '../actions';
+import { Link } from 'react-router-dom';
+import { Button } from 'antd';
 import PostCard from './PostCard';
 
 class Markets extends Component {
@@ -21,17 +23,32 @@ class Markets extends Component {
     return null;
   }
 
+  renderAddPostBtn() {
+    const { data: { isAdmin } } = this.props.currentUser;
+    if (isAdmin) {
+      return (
+        <Link to='/markets/new'>
+          <Button>Add New Post</Button>
+        </Link>
+      );
+    }
+
+    return null;
+  }
+
   render() {
     return (
       <div>
         {this.renderPosts()}
+        {this.renderAddPostBtn()}
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ posts }) => {
+const mapStateToProps = ({ currentUser, posts }) => {
   return {
+    currentUser,
     posts
   };
 };
--- routes/post_api_route.js
+++ routes/post_api_route.js
@@ -4,7 +4,7 @@ const Posts = mongoose.model('Post');
 module.exports = app => {
   app.get('/api/posts', async (req, res) => {
     try {
-      const posts = await Posts.find({ _user: req.user.id });
+      const posts = await Posts.find({});
       res.status(200).send(posts)
     } catch (e) {
       res.sendStatus(404);
